e2a016df5fba96d4d69891d0bb57d1c67883379f
Magnus Brange <magnus.brange@telavox.se>
2015-07-02 13:29:26 +0200

    Set the git command as an env var
    So that git hooks knows what is going on.

--- git-hooker.py
+++ git-hooker.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import subprocess
 
 # The first part of the input is the path to the script
 full_hook_path = sys.argv[0]
@@ -24,6 +25,9 @@ roots = next(os.walk(hook_dir, followlinks=True))[1]
 # Sort to have a guaranteed order
 roots = sorted(roots)
 
+git_command = os.popen("ps -ocommand= -p " + str(os.getppid())).read()
+os.environ["GIT_COMMAND"] = git_command
+
 # Search the roots for scripts with the same name as the hook and
 # run it with the given arguments
 for root in roots:
@@ -35,4 +39,7 @@ for root in roots:
       if f == hook and os.path.isfile(path) and os.access(path, os.X_OK):
         exit_code = os.system(path + " " + arguments)
         if exit_code != 0:
+          del os.environ["GIT_COMMAND"]
           sys.exit(exit_code)
+
+del os.environ["GIT_COMMAND"]
